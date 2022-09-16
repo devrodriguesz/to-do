d6c236daf30d07c59a531d7793b7c63f535de86b
devrodriguesz <dev.emersonrodrigues@outlook.com>
2022-09-16 07:48:43 -0300

    Adicionando a funcao que cancela a edicao e adicionando a mensagem de alerta que estã editando uma tarefa

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { 
-  View, 
-  Text, 
-  StyleSheet, 
-  SafeAreaView, 
-  TextInput, 
-  TouchableOpacity, 
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  TextInput,
+  TouchableOpacity,
   FlatList,
   Keyboard
 
@@ -15,11 +15,12 @@ import {
 
 import Login from './src/components/Login';
 import TaskList from './src/components/TaskList';
-import firebase from './src/services/firebaseConnection'
+import firebase from './src/services/firebaseConnection';
+import Feather from 'react-native-vector-icons/Feather';
 
 
 
-export default function App(){
+export default function App() {
 
   const [key, setKey] = useState('');
   const inputRef = useRef(null);
@@ -30,23 +31,23 @@ export default function App(){
 
 
   //buscar as tarefas que ja existem no banco de dados
-  useEffect(()=>{
-    function getUser(){
-      if(!user){
+  useEffect(() => {
+    function getUser() {
+      if (!user) {
         return;
       }
-      
-      firebase.database().ref('tarefas').child(user).once('value', (snapshot)=>{
+
+      firebase.database().ref('tarefas').child(user).once('value', (snapshot) => {
         setTasks([]);
 
-        snapshot?.forEach((childItem)=>{
-          let data={
+        snapshot?.forEach((childItem) => {
+          let data = {
             key: childItem.key,
             nome: childItem.val().nome
           }
-          
-          setTasks((oldTasks)=>[...oldTasks, data]);
-          
+
+          setTasks((oldTasks) => [...oldTasks, data]);
+
         })
       })
 
@@ -54,96 +55,113 @@ export default function App(){
 
     getUser();
 
-  },[user]);
+  }, [user]);
 
 
 
   // funçâo que cria um novo usuario e uma nova tarefa no banco de dados.
-  function handleAdd(){
-    if(newTask ===''){
+  function handleAdd() {
+    if (newTask === '') {
       return;
     }
 
     //Usuario edita uma tarefa
-    if(key!==''){
+    if (key !== '') {
       firebase.database().ref('tarefas').child(user).child(key).update({
         nome: newTask
       })
-      .then(()=>{
-        const taskIndex = tasks.findIndex( item=> item.key === key)
-        let taskClone = tasks;
-        taskClone[taskIndex].nome = newTask
+        .then(() => {
+          const taskIndex = tasks.findIndex(item => item.key === key)
+          let taskClone = tasks;
+          taskClone[taskIndex].nome = newTask
 
-        setTasks([...taskClone]);
+          setTasks([...taskClone]);
 
-      })
+        })
 
       Keyboard.dismiss();
       setNewTask('');
       setKey('');
       return;
     }
-    
+
     let tarefas = firebase.database().ref('tarefas').child(user);
     let chave = tarefas.push().key;
 
     tarefas.child(chave).set({
       nome: newTask
-    }).then( ()=> {
-      const data={
+    }).then(() => {
+      const data = {
         key: chave,
         nome: newTask
       };
-      
-    setTasks(oldTasks => [...oldTasks, data])    
+
+      setTasks(oldTasks => [...oldTasks, data])
     })
 
     Keyboard.dismiss();
     setNewTask('');
 
   }
-  
+
 
 
   //funçâo que deleta uma tarefa
-  function handleDelete(key){
+  function handleDelete(key) {
     firebase.database().ref('tarefas').child(user).child(key).remove()
-    .then(()=>{
-      const findTasks = tasks.filter( item => item.key !== key)
-      setTasks(findTasks);
-    })
+      .then(() => {
+        const findTasks = tasks.filter(item => item.key !== key)
+        setTasks(findTasks);
+      })
   }
 
 
 
   //funçâo que edita a tarefa
-  function handleEdit(data){
+  function handleEdit(data) {
     setKey(data.key);
     setNewTask(data.nome);
     inputRef.current.focus();
-    
+
+  }
+
+
+  // função que cancela a edição da tarefa
+  function cancelEdit(){
+    setKey('');
+    setNewTask('');
+    Keyboard.dismiss();
   }
 
 
 
   //condição se nãohouver usuário logado então, 
   //troca o componente Login para o componente cadastrar usuario.  
-  if (!user){
-    return <Login changeStatus={(user)=> setUser(user)}/>
+  if (!user) {
+    return <Login changeStatus={(user) => setUser(user)} />
   }
 
 
 
-  return(
+  return (
     <SafeAreaView style={styles.container}>
-     
+
+      { key.length>0 && 
+            <View style={{ flexDirection: 'row', marginBottom: 8,}}>
+            <TouchableOpacity onPress={cancelEdit}>
+              <Feather name="x-circle" size={20} color="#FF0000" />
+            </TouchableOpacity>
+            <Text style={{ marginLeft: 5, color: '#FF0000' }}>Você está editando uma tarefa</Text>
+          </View>
+      } 
+
       <View style={styles.containerTasks}>
         <TextInput
-        style={styles.input}
-        placeholder= "Digite aqui uma tarefa"
-        onChangeText={(text)=> setNewTask(text)}
-        value={newTask}
-        ref={inputRef}
+          style={styles.input}
+          placeholder="Digite aqui uma tarefa"
+          onChangeText={(text) => setNewTask(text)}
+          value={newTask}
+          ref={inputRef}
         />
         <TouchableOpacity style={styles.botaoAdd} onPress={handleAdd} >
           <Text style={styles.botaoText}>+</Text>
@@ -151,12 +169,12 @@ export default function App(){
       </View>
 
       <FlatList
-      data={tasks}
-      keyExtractor={(item)=> item.key}
-      renderItem={({item})=>(
-        <TaskList data={item} deleteItem={handleDelete} editItem={handleEdit}/>
-      )}
-      
+        data={tasks}
+        keyExtractor={(item) => item.key}
+        renderItem={({ item }) => (
+          <TaskList data={item} deleteItem={handleDelete} editItem={handleEdit} />
+        )}
+
       />
 
     </SafeAreaView>
@@ -164,26 +182,26 @@ export default function App(){
 }
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
+  container: {
+    flex: 1,
     paddingTop: 25,
     paddingHorizontal: 10,
     backgroundColor: '#f2f6fc'
   },
-  containerTasks:{
+  containerTasks: {
     flexDirection: 'row',
   },
-  input:{
-    flex:1,
-    marginBottom:10,
-    padding:10,
+  input: {
+    flex: 1,
+    marginBottom: 10,
+    padding: 10,
     backgroundColor: '#FFF',
     borderRadius: 4,
     borderWidth: 1,
     borderColor: '#141414',
     height: 45
   },
-  botaoAdd:{
+  botaoAdd: {
     backgroundColor: '#141414',
     height: 45,
     alignItems: 'center',
@@ -192,8 +210,8 @@ const styles = StyleSheet.create({
     marginLeft: 5,
     borderRadius: 4
   },
-  botaoText:{
-    color:'#FFF',
+  botaoText: {
+    color: '#FFF',
     fontSize: 22
   }
 
